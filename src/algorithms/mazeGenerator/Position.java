211fe08363dea3fbdc16f8040fd21b16067886c2
Omer Hanan <omer.hanan@gmail.com>
2022-04-22 13:47:12 +0300

    maze and position

--- src/algorithms/mazeGenerator/Position.java
+++ src/algorithms/mazeGenerator/Position.java
@@ -1,4 +1,13 @@
 package algorithms.mazeGenerator;
 
 public class Position {
+    // row property
+    private int row;
+    // col property
+    private int col;
+
+    // Getters & Setters
+    // equals
+    // hash code
+
 }
--- src/algorithms/mazeGenerator/maze.java
+++ src/algorithms/mazeGenerator/maze.java
@@ -1,4 +1,9 @@
 package algorithms.mazeGenerator;
 
 public class maze {
+    public static final int WALL =1;
+    public static final int TRAN =0;
+    private int[][] maze;
+    private Position start;
+    private Position goal;
 }
