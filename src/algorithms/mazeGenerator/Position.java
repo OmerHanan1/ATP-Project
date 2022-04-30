18f392ba56aaf64e12419331609729e7eb2ce1d0
Omer Hanan <omer.hanan@gmail.com>
2022-04-30 13:51:56 +0300

    Update position class

--- src/algorithms/mazeGenerator/Position.java
+++ src/algorithms/mazeGenerator/Position.java
@@ -6,6 +6,11 @@ public class Position {
     // col property
     private int col;
 
+    /**
+     * C-tor // given row-col.
+     * @param row: position's row. (represents by "i" in the grid).
+     * @param col: position's col. (represents by "j" in the grid).
+     */
     public Position(int row, int col){
         if (row < 0 || col < 0){
             // Prevent Illegal Argument Exceptions.
@@ -15,7 +20,40 @@ public class Position {
         this.row = row;
         this.col = col;
     }
-    // Getters & Setters
+
+    /**
+     * C-tor // copy constructor.
+     * @param position: the "other" to copy.
+     */
+    public Position(Position position){
+        if (position != null){
+            this.row=position.row;
+            this.col=position.col;
+        }
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) throws IllegalArgumentException{
+        if (row < 0){
+            throw new IllegalArgumentException("Row index must be greater or equal to 0");
+        }
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        if (col<0){
+            throw new IllegalArgumentException("Col index must be greater to equal to 0.");
+        }
+        this.col = col;
+    }
+
     // equals
     // hash code
 
