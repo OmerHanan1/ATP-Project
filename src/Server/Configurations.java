a97af43438ec198a3008f2a3a1a4ab202b17e5c4
Omer Hanan <omer.hanan@gmail.com>
2022-06-06 23:50:50 +0300

    Updated final parts - fixes will upload next commit

--- src/Server/Configurations.java
+++ src/Server/Configurations.java
@@ -1,6 +1,5 @@
 package Server;
 
-
 import algorithms.mazeGenerators.AMazeGenerator;
 import algorithms.mazeGenerators.MyMazeGenerator;
 import algorithms.mazeGenerators.SimpleMazeGenerator;
@@ -14,59 +13,55 @@ import java.io.InputStream;
 import java.util.Objects;
 import java.util.Properties;
 
-
 public class Configurations {
-    private static final Properties properties=new Properties();
-    private static Configurations instance=null;
+    private static final Properties properties = new Properties();
+    private static Configurations instance = null;
 
-    private Configurations()  {
-        InputStream inputStream=null;
+    private Configurations() {
+        InputStream inputStream = null;
         try {
             inputStream = getClass().getResourceAsStream("/config.properties");
 
             properties.load(inputStream);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static AMazeGenerator getGenerator(){
-        String p= properties.getProperty("mazeGeneratingAlgorithm");
-        AMazeGenerator mazeGenerator=null;
-        if (p.equals("MyMazeGenerator")){
-            mazeGenerator=new MyMazeGenerator();
+    public static AMazeGenerator getGenerator() {
+        String p = properties.getProperty("mazeGeneratingAlgorithm");
+        AMazeGenerator mazeGenerator = null;
+        if (p.equals("MyMazeGenerator")) {
+            mazeGenerator = new MyMazeGenerator();
         }
-        if(p.equals("SimpleMazeGenerator")){
-            mazeGenerator=new SimpleMazeGenerator();
+        if (p.equals("SimpleMazeGenerator")) {
+            mazeGenerator = new SimpleMazeGenerator();
         }
         return mazeGenerator;
     }
 
-    public static ASearchingAlgorithm getSearching(){
+    public static ASearchingAlgorithm getSearching() {
         ASearchingAlgorithm algorithm = null;
         assert false;
-        String p= properties.getProperty("mazeSearchingAlgorithm");
-        if (Objects.equals(p, "BestFirstSearch")){
-            algorithm=new BestFirstSearch();
+        String p = properties.getProperty("mazeSearchingAlgorithm");
+        if (Objects.equals(p, "BestFirstSearch")) {
+            algorithm = new BestFirstSearch();
         }
-        if (Objects.equals(p, "BreadthFirstSearch")){
-            algorithm=new BreadthFirstSearch();
-
+        if (Objects.equals(p, "BreadthFirstSearch")) {
+            algorithm = new BreadthFirstSearch();
         }
-        if (Objects.equals(p, "DepthFirstSearch")){
-            algorithm=new DepthFirstSearch();
-
+        if (Objects.equals(p, "DepthFirstSearch")) {
+            algorithm = new DepthFirstSearch();
         }
         return algorithm;
-
-        //return properties.getProperty("mazeSearchingAlgorithm");
     }
+
     public int getNumOfThreads() {
         return Integer.parseInt(properties.getProperty("threadPoolSize"));
     }
-    public static Configurations getInstance(){
-        if (instance==null){
+
+    public static Configurations getInstance() {
+        if (instance == null) {
             instance = new Configurations();
         }
         return instance;
